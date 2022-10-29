dcbd740dcbffb87b99e71f1f73cff6e9f548bd93
noora20FH <noora20fairy@gmail.com>
2022-10-29 22:25:47 +0700

    questions & score pages

---
+++ app/src/main/java/org/aplas/quizapplication/QuizResult.java
@@ -0,0 +1,33 @@
+package org.aplas.quizapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+/**
+ * Created by Azhar Rivaldi on 19/08/2018.
+ */
+
+public class QuizResult extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_result);
+
+        TextView hasil = (TextView)findViewById(R.id.hasil);
+        TextView nilai = (TextView)findViewById(R.id.nilai);
+
+        hasil.setText("Jawaban Benar :"+MainActivity.benar+"\nJawaban Salah :"+MainActivity.salah);
+        nilai.setText(""+MainActivity.hasil);
+    }
+
+    public void ulangi(View view){
+        finish();
+        Intent a = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(a);
+    }
+}
