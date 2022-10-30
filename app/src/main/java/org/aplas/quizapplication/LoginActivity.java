5548ae1eec4753e9ada85f5a39959c64db380b80
hildakhoirotul <99726877+hildakhoirotul@users.noreply.github.com>
2022-10-30 12:50:49 +0700

    add files LoginActivity

---
+++ app/src/main/java/org/aplas/quizapplication/LoginActivity.java
@@ -0,0 +1,38 @@
+package org.aplas.quizapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String username = "student";
+    String password = "student";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        final EditText txtUser = findViewById(R.id.txtUser);
+        final EditText txtPass = findViewById(R.id.txtPass);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(txtUser.getText().toString().equalsIgnoreCase(username)&& txtPass.getText().toString().equalsIgnoreCase(password)){
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }else{
+                    Toast.makeText(LoginActivity.this, "Username/Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
